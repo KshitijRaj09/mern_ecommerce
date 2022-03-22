be51369a38a6725c5401e03407cfe4c9d4d049e8
Kshitij Raj <kshitijraj@MACPRO87.local>
2022-03-23 02:23:02 +0530

    heroku setup serves.js file updated

--- server.js
+++ server.js
@@ -11,13 +11,6 @@ const PORT = process.env.PORT || 2000;
 
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join('ui_reactjs/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'ui_reactjs', 'build', 'index.html'));
-  });
-}
-
 app.use('/api', router.itemRouter);
 app.use('/api', router.cartRouter);
 app.use('/api', router.orderRouter);
@@ -29,6 +22,32 @@ async function connectMongoDB() {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, 'ui_reactjs', 'build')));
+      app.get('*', (req, res) => {
+        res.sendFile(
+          path.resolve(__dirname, 'ui_reactjs', 'build', 'index.html')
+        );
+      });
+    } else {
+      app.get('/', (req, res) => {
+        res.send('API is running');
+      });
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, 'ui_reactjs', 'build')));
+      app.get('*', (req, res) => {
+        res.sendFile(
+          path.resolve(__dirname, 'ui_reactjs', 'build', 'index.html')
+        );
+      });
+    } else {
+      app.get('/', (req, res) => {
+        res.send('API is running');
+      });
+    }
     app.listen(PORT, () => {
       console.log(
         `Server listening @ PORT ${PORT} && MongoDB running @ PORT ${connections[0].port})`
