6dcf7f9f8e28a0ff9993390c86661a9b9b3e1673
Kshitij Raj <kshitij.raj@kshitij.raj>
2023-05-28 00:28:57 +0530

    deployment to vercel

--- server.js
+++ server.js
@@ -16,33 +16,24 @@ app.use("/api", router.orderRouter);
 app.use("/api", router.userRouter);
 
 async function connectMongoDB() {
-   try {
-      const {connections} = await mongoose.connect(mongoURI, {
-         useNewUrlParser: true,
-         useUnifiedTopology: true,
-      });
+  try {
+    const {connections} = await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-      if (process.env.NODE_ENV === "production") {
-         app.use(express.static(path.join(__dirname, "ui_reactjs", "build")));
-         app.get("*", (req, res) => {
-            res.sendFile(
-               path.resolve(__dirname, "ui_reactjs", "build", "index.html")
-            );
-         });
-      } else {
-         app.get("/", (req, res) => {
-            res.send("API is running");
-         });
-      }
+    app.get("/", (req, res) => {
+      res.send("API is running");
+    });
 
-      app.listen(PORT, () => {
-         console.log(
-            `Server listening @ PORT ${PORT} && MongoDB running @ PORT ${connections[0].port})`
-         );
-      });
-   } catch (error) {
-      console.log(error);
-   }
+    app.listen(PORT, () => {
+      console.log(
+        `Server listening @ PORT ${PORT} && MongoDB running @ PORT ${connections[0].port})`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 connectMongoDB();
