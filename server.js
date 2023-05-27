d340da0ea4a5f1f8a20c89202f03272e176a8b12
Kshitij Raj <kshitij.raj@kshitij.raj>
2023-05-28 02:52:13 +0530

    deployment to vercel

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
 const router = require("./routes/index");
 const cors = require("cors");
 
